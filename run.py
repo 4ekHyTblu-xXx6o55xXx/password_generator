b62c319971e7e8e80c2a3b2a42d918d743e3725f
Ivan <levchenkoivan3007@gmail.com>
2026-07-16 15:29:01 +1000

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
+# from password_generator import PasswordGenerator
+#
+# gen = PasswordGenerator(length=20)
+# print(gen.generate())
 from password_generator import PasswordGenerator
-
-gen = PasswordGenerator(length=20)
-print(gen.generate())
